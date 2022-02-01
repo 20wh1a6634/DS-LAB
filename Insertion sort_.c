1daab4ce945fc0ddb5bf191bd22c4cff4d20c437
20wh1a6634-Tasneem <93579683+20wh1a6634@users.noreply.github.com>
2022-02-01 19:58:37 +0530

    Create Insertion sort_.c

---
+++ Insertion sort_.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int arr[100], n, c1, c2, temp;
+    printf("enter the no of elements\n");
+    scanf("%d", &n);
+    printf("enter the integers \n");
+    for(c1 = 0; c1 < n; c1++)
+    {
+        scanf("%d", &arr[c1]);
+    }
+    for(c1=1;c1 < n;c1++)
+    {
+        temp = arr[c1];
+        c2 = c1 - 1;
+        while((temp < arr[c2]) && (c2 >= 0))
+        {
+            arr[c2 + 1] = arr[c2];
+            c2 = c2 -1;
+        }
+        arr[c2 + 1] = temp;
+    }
+    printf("sorted list in ascending order\n");
+    for (c1 = 0; c1 < n; c1++)
+    {
+        printf("%d\n", arr[c1]);
+    }
+    return 0;
+}
