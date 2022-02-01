44eedb5fc9822dd32f5f13920e3304f3a14b8fbf
20wh1a6634-Tasneem <93579683+20wh1a6634@users.noreply.github.com>
2022-02-01 19:57:24 +0530

    Create Selection sort_.c

---
+++ Selection sort_.c
@@ -0,0 +1,29 @@
+int main()
+{
+    int arr[100], n, c1, c2, temp;
+    printf("enter the no of elements\n");
+    scanf("%d", &n);
+    printf("enter the integers \n");
+    for(c1 = 0; c1 < n; c1++)
+    {
+        scanf("%d", &arr[c1]);
+    }
+    for(c1=0;c1 < n;c1++)
+    {
+        for(c2 = c1+1; c2 < n; c2++)
+        {
+           if(arr[c1] > arr[c2])
+           {
+               temp = arr[c1];
+               arr[c1] = arr[c2];
+               arr[c2] = temp;
+           }
+        }
+    }
+    printf("sorted list in ascending order\n");
+    for (c1 = 0; c1 < n; c1++)
+    {
+        printf("%d\n", arr[c1]);
+    }
+    return 0;
+}
